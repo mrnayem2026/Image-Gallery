9475254aa3a4df47a49e51885ef486da812c7f23
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-11-04 13:56:21 +0600

    Deploy problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,47 @@ function App() {
   const images = [
     {
       id:"01",
-      url: '/public/image15.jpg',
+      url: 'https://i.ibb.co/YPCc8fv/image-1.webp',
     },
     {
       id:"02",
-      url: '/public/image16.png',
+      url: 'https://i.ibb.co/qJCy5vs/image-2.webp',
     },
     {
       id:"03",
-      url: '/public/image-3.webp',
+      url: 'https://i.ibb.co/dDq9vDn/image-3.webp',
     },
     {
       id:"04",
-      url: '/public/image-4.webp',
+      url: 'https://i.ibb.co/svcxG3s/image-4.webp',
     },
     {
       id:"05",
-      url: '/public/image-5.webp',
+      url: 'https://i.ibb.co/zb4szRG/image-5.webp',
     },
     {
       id:"06",
-      url: '/public/image-6.webp',
+      url: 'https://i.ibb.co/VwTbPKn/image-6.webp',
     },
     {
       id:"07",
-      url: '/public/image-7.webp',
+      url: 'https://i.ibb.co/N1s4mV1/image-7.webp',
     },
     {
       id:"08",
-      url: '/public/image-8.webp',
+      url: 'https://i.ibb.co/TmZc1Vw/image-8.webp',
     },
     {
       id:"09",
-      url: '/public/image-9.webp',
+      url: 'https://i.ibb.co/cNGV405/image-9.webp',
+    },
+    {
+      id:"10",
+      url: 'https://i.ibb.co/bB0vTzx/image-10.jpgs',
     },
     {
       id:"11",
-      url: '/public/image-10.jpeg',
-    },
-    {
-      id:"12",
-      url: '/public/image-11.jpeg',
+      url: 'https://i.ibb.co/ThRSD5R/image-11.jpg',
     },
   ];
   return (
